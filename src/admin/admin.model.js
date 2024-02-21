792d77eb115f304ed6a89ffac0cefaa7fd300798
jchanquin-2022240 <jchanquin-2022240@kinal.edu.gt>
2024-02-21 16:35:01 -0600

    Creation of the Admin with treir model,controller and routes

---
+++ src/admin/admin.model.js
@@ -0,0 +1,26 @@
+import mongoose from "mongoose";
+
+const AdminSchema = mongoose.Schema({
+    nombre: {
+        type: String,
+        requrired: [true, "El nombre es obligatorio"]
+    },
+    correo: {
+        type: String,
+        required: [true, "El correo es obligatorio"]
+    },
+    password: {
+        type: String,
+        required: [true, "La contraseña es obligatoria"]
+    },
+    role: {
+        type: String,
+        default: "ADMIN"
+    },
+    estado: {
+        type: Boolean,
+        default: true
+    }
+});
+
+export default mongoose.model('Admin', AdminSchema);
