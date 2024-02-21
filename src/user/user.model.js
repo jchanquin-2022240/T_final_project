b2665c498af737f6646f8160a17fcc23191a1ad3
jchanquin-2022240 <jchanquin-2022240@kinal.edu.gt>
2024-02-21 16:43:12 -0600

    Creation of the user, with your model, controller and route

---
+++ src/user/user.model.js
@@ -0,0 +1,26 @@
+import mongoose from "mongoose";
+
+const UserSchema = mongoose.Schema({
+    nombre:{
+        type: String,
+        required: [true,"El nombre es obligatorio"]
+    },
+    correo:{
+        type: String,
+        required: [true,"El correo es obligatorio"]
+    },
+    password:{
+        type:String,
+        required:[true,'La contraseña es obligatoria']
+    },
+    role: {
+        type: String,
+        default: "CLIENTE"
+    },
+    estado: {
+        type: Boolean,
+        default: true
+    }
+});
+
+export default mongoose.model('User', UserSchema);
