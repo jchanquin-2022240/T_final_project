c770fcef821c4980aa2ee5ea7bc26166546c4c26
jchanquin-2022240 <jchanquin-2022240@kinal.edu.gt>
2024-02-22 09:06:23 -0600

    creation endPoint Post

---
+++ src/admin/admin.controller.js
@@ -0,0 +1,18 @@
+import { response, request } from "express";
+import bcryptjs from 'bcryptjs';
+import Admin from './admin.model.js';
+
+
+export const adminPost = async (req = request, res = response ) => {
+    const{nombre ,correo ,password} = req.body;
+    const admin = new Admin( {nombre, correo, password});
+
+    const salt = bcriptjs.genSaltSync();
+    admin.password = bcryptjs.hashSync(password, salt);
+    
+    await admin.save();
+
+    res.status(200).json({
+        admin
+    });
+}
