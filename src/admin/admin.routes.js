aa2bd73c0568c0be1adabb0e89ec50f01247d574
jchanquin-2022240 <jchanquin-2022240@kinal.edu.gt>
2024-02-22 09:17:50 -0600

    Creation role and route of admin

---
+++ src/admin/admin.routes.js
@@ -0,0 +1,22 @@
+import { Router } from "express";
+import { check } from "express-validator";
+
+import {
+    adminPost,
+} from "./admin.controller.js";
+
+
+const router = Router;
+
+router.post(
+    "/:id",
+    [
+        check("nombre", "El nombre es obligatorio").not().isEmpty,
+        check("password", "El password debe ser mayor a 8 caracteres").isLength({min: 8}),
+        check("correo", "Este no es un correo válido").isEmail(),
+        check("role").custom(esRoleValido),
+        validarCampos,
+    ], adminPost);
+
+
+export default router;
