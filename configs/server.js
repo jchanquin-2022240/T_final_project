9f14d204fbb4d38e735ca9e2778153f94f24a66d
jchanquin-2022240 <jchanquin-2022240@kinal.edu.gt>
2024-02-26 09:42:14 -0600

    El login funciona para user falta admin y se agrego las collections al proyecto

--- configs/server.js
+++ configs/server.js
@@ -7,6 +7,7 @@ import morgan from 'morgan';
 import { dbConnection } from './mongo.js';
 import adminRoutes from '../src/admin/admin.routes.js';
 import userRoutes from '../src/user/user.routes.js';
+import authRoutes from '../src/auth/auth.routes.js';
 
 class Server {
     constructor() {
@@ -14,6 +15,7 @@ class Server {
         this.port = process.env.PORT;
         this.adminPath = '/management/v1/admin';
         this.userPath = '/management/v1/user';
+        this.authPath = '/management/v1/auth';
 
         this.conectarDB();
         this.middlewares();
@@ -33,6 +35,7 @@ class Server {
     }
 
     routes() {
+        this.app.use(this.authPath, authRoutes);
         this.app.use(this.adminPath, adminRoutes);
         this.app.use(this.userPath, userRoutes);
     }
