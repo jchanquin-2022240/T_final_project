a68c724af6386867d7b54a42e3e386ec697dbf40
jchanquin-2022240 <jchanquin-2022240@kinal.edu.gt>
2024-03-08 23:10:38 -0600

    updates

--- src/shoppingCart/shoppingCart.routes.js
+++ src/shoppingCart/shoppingCart.routes.js
@@ -2,6 +2,7 @@ import { Router } from 'express';
 import { check } from 'express-validator';
 import { addShoppingCart } from './shoppingCart.controller.js';
 import { validateJWT } from '../middlewares/validar-jwt.js';
+import { esClienteRole } from '../middlewares/validar-role.js';
 
 const router = Router();
 
@@ -9,6 +10,7 @@ router.post(
     "/add",
     [
         validateJWT,
+        esClienteRole,
         check("id", "Id is required").not().isEmpty(),
         check("nombre", "Name is required").not().isEmpty(),
         check("quantity", "Quantity is required").not().isEmpty(),
